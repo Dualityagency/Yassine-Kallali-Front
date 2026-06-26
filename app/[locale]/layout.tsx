0d9948a8fe100a3bb3bcf2485a4f066f9146eb3a
TOUMIOmar <omartoumi158@gmail.com>
2026-06-26 18:12:14 +0100

    feat:update en/ar/it.json and add genrateMetadata in layout  v1.0.2 - Add en.json with full English translation - Add it.json with full Italian translation - Add ar.json with full Arabic translation (RTL support) - Implement generateMetadata function with multi-language SEO support - Add page-specific metadata helpers for all locales - Set up language alternates (en, fr, ar, it)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,7 +3,73 @@ import "../globals.css";
 
 import { NextIntlClientProvider } from "next-intl";
 import Footer from "@/components/layout/Footer";
+import { getTranslations } from "next-intl/server";
+import { Metadata } from "next";
 
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  const t = await getTranslations({
+    locale,
+    namespace: "MetaData",
+  });
+
+  const keywords: string[] = t.raw("keywords") as string[];
+
+  return {
+    title: {
+      template: "%s | Dr. Yassine Kallali",
+      default: t("title"),
+    },
+    description: t("description"),
+    keywords,
+
+    authors: [
+      {
+        name: "Dr. Yassine Kallali",
+        url: "https://www.dr-yassine-kallali.com",
+      },
+    ],
+
+    applicationName: "Dr. Yassine Kallali",
+
+    alternates: {
+      canonical: `https://www.dryassinekallali.com/${locale}`,
+      languages: {
+        en: "https://www.dryassinekallali.com/en",
+        fr: "https://www.dryassinekallali.com/fr",
+        ar: "https://www.dryassinekallali.com/ar",
+        it: "https://www.dryassinekallali.com/it",
+      },
+    },
+
+    openGraph: {
+      title: t("title"),
+      description: t("description"),
+      url: `https://www.dryassinekallali.com.com/${locale}`,
+      siteName: "Dr. Yassine Kallali",
+      locale:
+        locale === "ar"
+          ? "ar_TN"
+          : locale === "fr"
+          ? "fr_TN"
+          : locale === "it"
+          ? "it_IT"
+          : "en_US",
+      type: "website",
+    },
+
+    icons: {
+      icon: "/logo_black.png",
+      apple: "/logo_black.png",
+    },
+  };
+}
 
 export default function LocaleLayout({
   children,
