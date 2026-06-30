44f0b5d2507e0524552f95200b230362ae30f897
TOUMIOmar <omartoumi158@gmail.com>
2026-06-30 18:05:45 +0100

    feat: internationalize implant page and update contact page UI
    feat(PrendreContact):
    - Add PrendreContact translations to fr, en, ar, and it locale files
    - Replace static PrendreContact content with next-intl translations
    - Update the mobile background image position to `bg-center`
    
    feat(Info):
    - Update the Contact Info component background image  for the map

--- components/contact/Info.tsx
+++ components/contact/Info.tsx
@@ -1,76 +1,115 @@
-import { cn } from "@/lib/utils"
-import { useTranslations } from "next-intl"
-import Image from "next/image"
+import { cn } from "@/lib/utils";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
+export default function Info() {
+  const t = useTranslations("contact");
+  const schedules = t.raw("schedules.list") as {
+    title: string;
+    value: string;
+  }[];
 
+  return (
+    <div className="w-full lg:w-[40.8%] flex flex-col gap-y-6.25 lg:gap-y-4.75">
+      {/* Coordonnées */}
+      <div className="bg-primary pt-10.75 pb-8.5 lg:pb-10 pl-4.5 lg:pl-8.75 pr-6 lg:pr-9 rounded-[15px]">
+        <h2 className="title3 text-white! mb-5.5">
+          {t("contactDetails.title")}
+        </h2>
 
-export default function Info(){
-
-    const t = useTranslations('contact')
-    const schedules= t.raw('schedules.list') as {title:string,value:string}[]
-
-    return(
-
-        <div className="w-full lg:w-[40.8%] flex flex-col gap-y-6.25 lg:gap-y-4.75">
-
-
-            {/* Coordonnées */}
-            <div className="bg-primary pt-10.75 pb-8.5 lg:pb-10 pl-4.5 lg:pl-8.75 pr-6 lg:pr-9 rounded-[15px]">
-                <h2 className="title3 text-white! mb-5.5">{t('contactDetails.title')}</h2>
-                
-                <div className="flex flex-col gap-y-3 lg:gap-y-3.5">
-                <div className="flex items-center gap-x-4">
-                    <Image src={"/icons/location.png"} width={16} height={20} className="w-4 h-fit" alt="location icon"/>
-                    <div className="flex flex-col gap-y-0.5">
-                        <h3 className="font-semibold text-white! uppercase text-[14px]">{t('contactDetails.address-title')}</h3>
-                        <p className="paragraph2 text-white!">{t('contactDetails.address-value')}</p>
-                    </div>
-                </div>
-
-
-                <div className="flex items-center gap-x-4">
-                    <Image src={"/icons/phone.png"} width={16} height={20} className="w-5 h-fit" alt="location icon"/>
-                    <div className="flex flex-col gap-y-0.5">
-                        <h3 className="font-semibold text-white! uppercase text-[14px]">{t('contactDetails.phoneNumber-title')}</h3>
-                        <p className="paragraph2 text-white!">{t('contactDetails.phoneNumber-value')}</p>
-                    </div>
-                </div>
-
-
-                <div className="flex items-center gap-x-4">
-                    <Image src={"/icons/email.png"} width={16} height={20} alt="location icon"/>
-                    <div className="flex flex-col gap-y-0.5">
-                        <h3 className="font-semibold text-white! uppercase text-[14px]">{t('contactDetails.email-title')}</h3>
-                        <p className="paragraph2 text-white!">{t('contactDetails.email-value')}</p>
-                    </div>
-                </div>
-                </div>
+        <div className="flex flex-col gap-y-3 lg:gap-y-3.5">
+          <div className="flex items-center gap-x-4">
+            <Image
+              src={"/icons/location.png"}
+              width={16}
+              height={20}
+              className="w-4 h-fit"
+              alt="location icon"
+            />
+            <div className="flex flex-col gap-y-0.5">
+              <h3 className="font-semibold text-white! uppercase text-[14px]">
+                {t("contactDetails.address-title")}
+              </h3>
+              <p className="paragraph2 text-white!">
+                {t("contactDetails.address-value")}
+              </p>
             </div>
+          </div>
 
-            {/* Horaires */}
-            <div className="border border-border rounded-[15px] px-4.5 lg:pl-8.75 lg:pr-7.25 py-10.75 flex flex-col gap-y-5">
-                    <h2 className="title3">{t('schedules.title')}</h2>
-                    <div className="flex flex-col gap-y-3">
-                    {schedules.map((el,index)=>
-                    (
-                    <div className="flex flex-row justify-between" key={index}>
-                    <h3 className="paragraph2">{el?.title}</h3> 
-                    <h4 className={cn('title4',index ==schedules.length-1 && "text-[#BA1A1A]!")}>{el?.value}</h4>
-                    </div>
-                    )
-                    )}
-                    </div>
+          <div className="flex items-center gap-x-4">
+            <Image
+              src={"/icons/phone.png"}
+              width={16}
+              height={20}
+              className="w-5 h-fit"
+              alt="location icon"
+            />
+            <div className="flex flex-col gap-y-0.5">
+              <h3 className="font-semibold text-white! uppercase text-[14px]">
+                {t("contactDetails.phoneNumber-title")}
+              </h3>
+              <p className="paragraph2 text-white!">
+                {t("contactDetails.phoneNumber-value")}
+              </p>
             </div>
+          </div>
 
-            {/* Map */}
-            <div className="h-[234px] lg:h-[361px] px-3 py-9.5 bg-background rounded-[17px] ">
-                    <div className="relative flex justify-center w-full h-full  bg-[url(/contact/map.png)] bg-contain lg:bg-cover bg-no-repeat">
-                            <h1 className="absolute top-[60%] w-[70%] font-semibold text-gray text-[12px] leading-[100%] text-center">
-                                {t('address')}
-                            </h1>
-                    </div>
+          <div className="flex items-center gap-x-4">
+            <Image
+              src={"/icons/email.png"}
+              width={16}
+              height={20}
+              alt="location icon"
+            />
+            <div className="flex flex-col gap-y-0.5">
+              <h3 className="font-semibold text-white! uppercase text-[14px]">
+                {t("contactDetails.email-title")}
+              </h3>
+              <p className="paragraph2 text-white!">
+                {t("contactDetails.email-value")}
+              </p>
             </div>
+          </div>
         </div>
+      </div>
 
-    )
+      {/* Horaires */}
+      <div className="border border-border rounded-[15px] px-4.5 lg:pl-8.75 lg:pr-7.25 py-10.75 flex flex-col gap-y-5">
+        <h2 className="title3">{t("schedules.title")}</h2>
+        <div className="flex flex-col gap-y-3">
+          {schedules.map((el, index) => (
+            <div className="flex flex-row justify-between" key={index}>
+              <h3 className="paragraph2">{el?.title}</h3>
+              <h4
+                className={cn(
+                  "title4",
+                  index == schedules.length - 1 && "text-[#BA1A1A]!",
+                )}
+              >
+                {el?.value}
+              </h4>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Map */}
+      <div className="h-[234px] lg:h-[361px] px-[7.78px] lg:px-3 py-6 lg:py-9.5 bg-background rounded-[17px] ">
+        <div className="relative flex justify-center w-full h-full  bg-[url(/contact/map.png)] bg-contain lg:bg-cover bg-no-repeat">
+          <div className="absolute top-[40%] w-[70%] flex flex-col gap-y-2.25 lg:gap-y-3.5 items-center">
+            <Image
+              src={"/icons/locationBlue.png"}
+              width={23}
+              height={23}
+              className="w-[14.78px] h-[14.78px] lg:w-[23px] lg:h-[23px]"
+              alt="icon location"
+            />
+            <h1 className="font-semibold text-gray text-[12px] leading-[100%] text-center">
+              {t("address")}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/implant-immediat/PrendreContact.tsx
+++ components/implant-immediat/PrendreContact.tsx
@@ -1,43 +1,33 @@
 import Link from "next/link";
 import { Badge } from "../ui/badge";
 import CtaButton from "../ui/CtaButton";
+import { useTranslations } from "next-intl";
 
 export default function PrendreContact() {
+  const t = useTranslations("implant-immediat.contact");
+  const h3List = t.raw("h3List") as string[];
   return (
     <div
-      className="bg-[url(/home/prendreContact.webp)] bg-cover bg-no-repeat
+      className="bg-[url(/home/prendreContact.webp)] bg-cover bg-center lg:bg-top-left bg-no-repeat
         px-6 lg:px-20 py-12 lg:py-16.5 
         "
     >
       <Badge variant={"tertiary"} className="mb-6 lg:mb-9.5">
-        Prendre contact
+        {t("badge")}
       </Badge>
 
       <div className="flex flex-col lg:flex-row justify-between items-end gap-y-6 mb-9 lg:mb-10.25">
-        <div className="w-full lg:w-[40%] flex flex-col gap-y-6 lg:gap-y-10.25">
-          <h2 className="title1 ">Prêt pour votre nouveau sourire ?</h2>
-          <p className="paragraph2 text-white!">
-            Nos experts vous accompagnent de la première consultation virtuelle
-            jusqu'au résultat final à Sousse.
-          </p>
+        <div className="w-f ull lg:w-[40%] flex flex-col gap-y-6 lg:gap-y-10.25">
+          <h2 className="title1 ">{t("title")}</h2>
+          <p className="paragraph2 text-white!">{t("description")}</p>
         </div>
 
         <div className="flex flex-col gap-y-1.5 lg:gap-y-1.75">
-          <h3 className="font-medium text-white lg:text-[17px] leading-[100%]">
-            Réponse sous 48h
-          </h3>
-          <h3 className="font-medium text-white lg:text-[17px] leading-[100%]">
-            Aucun engagement
-          </h3>
-          <h3 className="font-medium text-white text-[14.43px] lg:text-[17px] leading-[100%]">
-            Expertise en implantologie avancée
-          </h3>
-          <h3 className="font-medium text-white lg:text-[17px] leading-[100%]">
-            Séjour optimisé de 2 à 5 jours
-          </h3>
-          <h3 className="font-medium text-white lg:text-[17px] leading-[100%]">
-            Suivi depuis votre pays de résidence
-          </h3>
+          {h3List.map((el, index) => (
+            <h3 key={index} className="font-medium text-white text-[14.43px] lg:text-[17px] leading-[100%]">
+              {el}
+            </h3>
+          ))}
         </div>
       </div>
 
@@ -46,14 +36,12 @@ export default function PrendreContact() {
             px-5.25 lg:px-5 py-4 lg:py-5.5
             "
       >
-        <h3 className="w-full lg:w-[26%] font-medium text-white text-[15px] lg:text-[17px] leading-[100%] capitalize">
-          Découvrez si vous êtes éligible à un implant immédiat en moins de 48h.
-        </h3>
+        <h4 className="w-full lg:w-[26%] font-medium text-white text-[15px] lg:text-[17px] leading-[100%] capitalize">
+          {t("h4")}
+        </h4>
 
         <Link href={"/contact"}>
-          <CtaButton variant="white">
-            Demander une évaluation gratuite
-          </CtaButton>
+          <CtaButton variant="white">{t("ctaButton")}</CtaButton>
         </Link>
       </div>
     </div>
