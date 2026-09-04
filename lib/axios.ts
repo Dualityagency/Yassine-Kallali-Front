26d9b6fca80b7ad22deb953b98eba89849fb260a
TOUMIOmar <omartoumi158@gmail.com>
2026-09-04 17:04:08 +0100

    fix: update axios baseURL to use environment variable for API URL

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
